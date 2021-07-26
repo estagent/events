0a0d62c42dc5f62a4c44fb89b966d38f1025322b
estagent <estagent@gmail.com>
2021-07-26 03:29:48 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import {listenEvents, getEventDetail, getEventUser, mergeEvents, Events} from './events'
+export {
+    getEventDetail,
+    getEventUser,
+    listenEvents,
+    Events,
+    mergeEvents,
+}
