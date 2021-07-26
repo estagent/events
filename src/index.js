88cdbb19de63c216fc8812fa2367153cf7eec0b2
estagent <estagent@gmail.com>
2021-07-26 04:20:23 +0300

    boot added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import {listenEvents, getEventDetail, getEventUser, mergeEvents, Events} from './events'
+
+const bootEvents = () => {
+    return {
+        listenEvents: listenEvents,
+        events: () => Events,
+    }
+}
 export {
     getEventDetail,
     getEventUser,
     listenEvents,
     Events,
     mergeEvents,
+    bootEvents
 }
