f619ccb10d82384d36a412e7415adb9de7950b81
estagent <estagent@gmail.com>
2022-03-16 15:27:53 +0300

    5.0.7

---
+++ src/dispacthers.js
@@ -0,0 +1,47 @@
+import Events from './events'
+
+export const dispatchUserUnmounted = () => {
+  window.dispatchEvent(new Event(Events.UserUnmounted))
+}
+
+export const dispatchUserMounted = detail => {
+  window.dispatchEvent(new CustomEvent(Events.UserMounted, {detail: detail}))
+}
+
+export const dispatchUserAuthenticated = detail => {
+  window.dispatchEvent(
+    new CustomEvent(Events.UserAuthenticated, {detail: detail}),
+  )
+}
+export const dispatchUserSignedOut = () => {
+  window.dispatchEvent(new Event(Events.UserSignedOut))
+}
+
+export const dispatchUserUpdated = detail => {
+  window.dispatchEvent(new CustomEvent(Events.UserUpdated, {detail: detail}))
+}
+
+/**
+ * Session events
+ */
+
+export const dispatchSessionMounted = () => {
+  window.dispatchEvent(new Event(Events.SessionMounted))
+}
+
+export const dispatchSessionRefreshed = detail => {
+  window.dispatchEvent(
+    new CustomEvent(Events.SessionRefreshed, {detail: detail}),
+  )
+}
+
+
+export const dispatchSessionInitialised = () => {
+  window.dispatchEvent(new Event(Events.SessionInitialized))
+}
+export const dispatchSessionCreated = () => {
+  window.dispatchEvent(new Event(Events.SessionCreated))
+}
+export const dispatchSessionDestroyed = () => {
+  window.dispatchEvent(new Event(Events.SessionDestroyed))
+}
