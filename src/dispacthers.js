21283320cebd6f3faff0d957ba784a937b4fb220
estagent <estagent@gmail.com>
2022-03-18 10:45:32 +0300

    5.6.7 - misc callbacks

--- src/dispacthers.js
+++ src/dispacthers.js
@@ -4,33 +4,20 @@ export const Boot = () =>
     window.dispatchEvent(new Event(Events.AppBoot))
 
 
-export const Initialised = (App) => {
-    window.dispatchEvent(
-        new CustomEvent(Events.AppInitialised, {detail: {App: App}}),
-    )
-}
+export const Initialised = (App) => window.dispatchEvent(
+    new CustomEvent(Events.AppInitialised, {detail: {App: App}}),
+)
+
+export const UserUnmounted = () => window.dispatchEvent(new Event(Events.UserUnmounted))
+
+export const UserMounted = detail => window.dispatchEvent(new CustomEvent(Events.UserMounted, {detail: detail}))
+
+export const UserChanged = detail => window.dispatchEvent(new CustomEvent(Events.UserChanged, {detail: detail}))
 
 
-
-export const UserUnmounted = () => {
-    window.dispatchEvent(new Event(Events.UserUnmounted))
-}
-
-export const UserMounted = detail => {
-    window.dispatchEvent(new CustomEvent(Events.UserMounted, {detail: detail}))
-}
-
-export const UserChanged = detail => {
-    window.dispatchEvent(new CustomEvent(Events.UserChanged, {detail: detail}))
-}
-
-
-export const UserAuthenticated = detail => {
-    window.dispatchEvent(
-        new CustomEvent(Events.UserAuthenticated, {detail: detail}),
-    )
-}
-
+export const UserAuthenticated = detail => window.dispatchEvent(
+    new CustomEvent(Events.UserAuthenticated, {detail: detail}),
+)
 
 export const WalletExpired = () =>
     window.dispatchEvent(new Event(Events.WalletExpired))
@@ -49,46 +36,32 @@ export const WalletUpdated = (wallet) =>
         }
     }))
 
-export const UserSignedOut = () => {
-    window.dispatchEvent(new Event(Events.UserSignedOut))
-}
+export const UserSignedOut = () => window.dispatchEvent(new Event(Events.UserSignedOut))
 
 
-export const SessionMounted = () => {
-    window.dispatchEvent(new Event(Events.SessionMounted))
-}
+export const SessionMounted = () => window.dispatchEvent(new Event(Events.SessionMounted))
 
-export const SessionRefreshed = detail => {
-    window.dispatchEvent(
-        new CustomEvent(Events.SessionRefreshed, {detail: detail}),
-    )
-}
-export const TokenMismatched = (response) => {
-    window.dispatchEvent(
-        new CustomEvent(Events.TokenMismatched, {
-            detail: {
-                response: response,
-            },
-        }),
-    )
-}
-export const AuthorizationFailed = (response) => {
-    window.dispatchEvent(
-        new CustomEvent(Events.AuthorizationFailed, {
-            detail: {
-                response: response,
-            },
-        }),
-    )
-}
+export const SessionRefreshed = detail => window.dispatchEvent(
+    new CustomEvent(Events.SessionRefreshed, {detail: detail}),
+)
+
+export const TokenMismatched = (response) => window.dispatchEvent(
+    new CustomEvent(Events.TokenMismatched, {
+        detail: {
+            response: response,
+        },
+    }),
+)
+
+export const AuthorizationFailed = (response) => window.dispatchEvent(
+    new CustomEvent(Events.AuthorizationFailed, {
+        detail: {
+            response: response,
+        },
+    }),
+)
 
 
-export const SessionInitialised = () => {
-    window.dispatchEvent(new Event(Events.SessionInitialized))
-}
-export const SessionCreated = () => {
-    window.dispatchEvent(new Event(Events.SessionCreated))
-}
-export const SessionDestroyed = () => {
-    window.dispatchEvent(new Event(Events.SessionDestroyed))
-}
+export const SessionInitialised = () => window.dispatchEvent(new Event(Events.SessionInitialized))
+export const SessionCreated = () => window.dispatchEvent(new Event(Events.SessionCreated))
+export const SessionDestroyed = () => window.dispatchEvent(new Event(Events.SessionDestroyed))
